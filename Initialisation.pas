80fa2cec96d9a9a2730e4c3213e120743b576009
Alban <albanvacher76@gmail.com>
2025-10-07 16:09:34 +0200

    test

---
+++ Initialisation.pas
@@ -0,0 +1,14 @@
+unit Initialisation
+
+interface
+
+uses TypeUnit;
+
+procedure InitialiserPlateau(var Plateau: TPlateau);
+procedure InitialiserPieces(var Pieces: array of TPiece; var Plateau: TPlateau);
+
+
+
+implementation
+
+end.
